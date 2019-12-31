e8508fda5b7273bfdb994d4763c407b669143948
Timon Masberg <kontakt@timonmasberg.de>
2019-12-31 12:10:15 +0100

    Update db.php

--- backend/db.php
+++ backend/db.php
@@ -1,9 +1,9 @@
 <?php
 
-$dbname = "db242539x2492164";
-$dbpw = "jugendhackt";
-$dburl = "mysql.webhosting61.1blu.de";
-$dbuser = "s242539_2492164";
+$dbname = "";
+$dbpw = "";
+$dburl = "";
+$dbuser = "";
 
 $con = mysqli_connect($dburl,$dbuser,$dbpw,$dbname);
 $con->set_charset("utf8");
